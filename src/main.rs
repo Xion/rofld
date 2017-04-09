91a7de11e7ca375373ee5f09856fd9d72996ffe4
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2017-04-09 18:25:48 +0100

    Hello HTTP!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+//!
+//! rofld  -- Lulz on demand
+//!
+
+extern crate futures;
+extern crate hyper;
+
+
+use futures::future::{self, FutureResult};
+use hyper::{Get, StatusCode};
+use hyper::server::{Http, Service, Request, Response};
+
+
+const HOST: &'static str = "0.0.0.0";
+const PORT: u16 = 1337;
+
+
 fn main() {
-    println!("Hello, world!");
+    let addr = format!("{}:{}", HOST, PORT).parse().unwrap();
+    let server = Http::new().bind(&addr, || Ok(Rofl)).unwrap();
+    server.run().unwrap();
+}
+
+
+/// Hyper async service implementing all the functionality.
+pub struct Rofl;
+
+impl Service for Rofl {
+    type Request = Request;
+    type Response = Response;
+    type Error = hyper::Error;
+    type Future = FutureResult<Response, hyper::Error>;
+
+    fn call(&self, req: Request) -> Self::Future {
+        let result = match (req.method(), req.path()) {
+            (&Get, "/") => Response::new().with_body("Hello world".as_bytes()),
+            _ => Response::new().with_status(StatusCode::NotFound),
+        };
+        future::ok(result)
+    }
 }
