735bbfcb79e259ce9dc002b9457c8c7675f9d2f0
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2017-04-13 23:00:17 +0100

    Text rendering. I mean, look what I've accomplished

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,11 @@
              extern crate isatty;
 #[macro_use] extern crate lazy_static;
 #[macro_use] extern crate maplit;
+             extern crate rusttype;
              extern crate serde;
 #[macro_use] extern crate serde_derive;
 #[macro_use] extern crate serde_json;
+             extern crate serde_qs;
              extern crate slog_envlogger;
              extern crate slog_stdlog;
              extern crate slog_stream;
@@ -31,21 +33,30 @@ mod logging;
 mod templates;
 
 
+use std::env;
 use std::error::Error;
 use std::fmt;
-use std::io::{self, Write};
+use std::fs;
+use std::io::{self, Read, Write};
+use std::path::PathBuf;
 
 use futures::Future;
 use futures::future::{self, BoxFuture};
 use hyper::{Get, Post, StatusCode};
 use hyper::server::{Http, Service, Request, Response};
-use image::GenericImage;
+use image::{GenericImage, Rgb};
+use rusttype::{FontCollection, Point, Scale};
 
 use ext::hyper::BodyExt;
 
 
 const HOST: &'static str = "0.0.0.0";
 const PORT: u16 = 1337;
+
+lazy_static! {
+    static ref FONT_DIR: PathBuf =
+        env::current_dir().unwrap().join("data").join("fonts");
+}
 
 
 fn main() {
@@ -74,7 +85,7 @@ impl Service for Rofl {
         self.log(&req);
 
         match (req.method(), req.path()) {
-            (&Post, "/caption") => return self.handle_caption(req),
+            (_, "/caption") => return self.handle_caption(req),
             (&Get, "/templates") => return self.handle_list_templates(req),
             _ => {},
         }
@@ -91,14 +102,27 @@ impl Service for Rofl {
 impl Rofl {
     /// Handle the image captioning request.
     fn handle_caption(&self, request: Request) ->  <Self as Service>::Future {
-        request.body().into_bytes().map(|bytes| {
-            trace!("Decoding image macro spec from {} bytes of JSON", bytes.len());
-            let im: ImageMacro = match serde_json::from_reader(&*bytes) {
+        let (method, url, _, _, body) = request.deconstruct();
+        body.into_bytes().map(move |bytes| {
+            let parsed_im: Result<_, Box<Error>> = match method {
+                Get => {
+                    trace!("Decoding image macro spec from {} bytes of query string",
+                        url.query().map(|q| q.len()).unwrap_or(0));
+                    serde_qs::from_str(url.query().unwrap_or("")).map_err(Into::into)
+                },
+                Post => {
+                    trace!("Decoding image macro spec from {} bytes of JSON", bytes.len());
+                    serde_json::from_reader(&*bytes).map_err(Into::into)
+                },
+                _ => return Response::new().with_status(StatusCode::MethodNotAllowed),
+            };
+
+            let im: ImageMacro = match parsed_im {
                 Ok(im) => im,
                 Err(e) => {
-                    error!("Failed to decode image macro JSON: {}", e);
+                    error!("Failed to decode image macro: {}", e);
                     return error_response(
-                        StatusCode::BadRequest, "cannot decode JSON request");
+                        StatusCode::BadRequest, "cannot decode request");
                 },
             };
             debug!("Decoded {:?}", im);
@@ -142,6 +166,8 @@ struct ImageMacro {
     template: String,
     width: Option<u32>,
     height: Option<u32>,
+
+    font: Option<String>,
     top_text: Option<String>,
     middle_text: Option<String>,
     bottom_text: Option<String>,
@@ -162,9 +188,32 @@ impl ImageMacro {
         let target_height = self.height.unwrap_or(orig_height);
         debug!("Resizing template `{}` from {}x{} to {}x{}",
             self.template, orig_width, orig_height, target_width, target_height);
-        let img = img.resize(target_width, target_height, image::FilterType::Lanczos3);
+        let mut img = img.resize(target_width, target_height, image::FilterType::Lanczos3);
 
-        // TODO: render the text
+        // TODO: other texts and better
+        if let Some(ref bottom_text) = self.bottom_text {
+            let font_name = self.font.as_ref().map(|s| s.as_str()).unwrap_or("Impact");
+            let font_file = fs::File::open(FONT_DIR.join(format!("{}.ttf", font_name)))
+                .map_err(CaptionError::Font)?;
+            let font_bytes: Vec<_> = font_file.bytes().map(Result::unwrap).collect();
+            let font = FontCollection::from_bytes(&*font_bytes).into_font().unwrap();
+            let size = 64.0;
+            let pos = Point{x: 0.0, y: target_height as f32 - size};
+            font.layout(bottom_text, Scale::uniform(size), pos)
+                .scan(0.0, |caret, g| {
+                    g.draw(|x, y, v| {
+                        let x = (pos.x + *caret) as u32 + x;
+                        let y = pos.y as u32 + y;
+                        let v = (v * 255f32) as u8;
+                        img.as_mut_rgb8().unwrap().put_pixel(x, y, Rgb{data: [v, v, v]});
+                    });
+                    // TODO: probably need to use advance_width + font.pair_kerning instead
+                    let rect = g.pixel_bounding_box().unwrap();
+                    *caret += rect.width() as f32;
+                    Some(())
+                })
+                .count();  // just to consume the iterator
+        }
 
         let (width, height) = img.dimensions();
         image::png::PNGEncoder::new(writer)
@@ -202,6 +251,7 @@ impl fmt::Debug for ImageMacro {
 #[derive(Debug)]
 enum CaptionError {
     Template(String),
+    Font(io::Error),
     Encode(io::Error),
 }
 impl CaptionError {
@@ -209,6 +259,7 @@ impl CaptionError {
     pub fn status_code(&self) -> StatusCode {
         match *self {
             CaptionError::Template(..) => StatusCode::NotFound,
+            CaptionError::Font(..) => StatusCode::NotFound,
             CaptionError::Encode(..) => StatusCode::InternalServerError,
         }
     }
@@ -226,6 +277,7 @@ impl fmt::Display for CaptionError {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
         match *self {
             CaptionError::Template(ref t) => write!(fmt, "cannot find template `{}`", t),
+            CaptionError::Font(ref t) => write!(fmt, "cannot find font `{}`", t),
             CaptionError::Encode(ref e) => write!(fmt, "failed to encode the  final image: {}", e),
         }
     }
