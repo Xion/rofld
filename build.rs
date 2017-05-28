1f227eddd8c82ba0555823f2e44ad6f9333a3712
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2017-05-28 20:54:34 +0100

    Look at my rustc, my rustc is amazing

--- build.rs
+++ build.rs
@@ -1,33 +1,38 @@
 //! Le build script.
 
+extern crate rustc_version;
+
+
 use std::env;
 use std::error::Error;
 use std::fs::File;
-use std::io::Write;
+use std::io::{self, Write};
 use std::path::Path;
 use std::process::Command;
 use std::str;
 
 
-/// File in the $OUT_DIR where the current revision is written.
-const REVISION_FILE: &'static str = "revision";
-
-
 fn main() {
-    // Obtain Git SHA to pass it further to the crate code via ad-hoc file.
-    let mut revision_file = {
-        // We cannot pass the SHA as an env!() variable to the crate code,
-        // so the workaround is to write it to a file for include_str!().
-        // Details: https://github.com/rust-lang/cargo/issues/2875
-        let out_dir = env::var("OUT_DIR").unwrap();
-        let rev_path = Path::new(&out_dir).join(REVISION_FILE);
-        File::create(&rev_path).unwrap()
-    };
     match git_head_sha() {
-        Ok(rev) => revision_file.write_all(&rev.into_bytes()).unwrap(),
+        Ok(rev) => pass_metadata("REVISION", &rev).unwrap(),
         Err(e) => println!("cargo:warning=Failed to obtain current Git SHA: {}", e),
     };
+    match compiler_signature() {
+        Ok(sig) => pass_metadata("COMPILER", &sig).unwrap(),
+        Err(e) => println!("cargo:warning=Failed to obtain compiler information: {}", e),
+    };
 }
+
+fn pass_metadata<P: AsRef<Path>>(kind: P, data: &str) -> io::Result<()> {
+    // We cannot data as an env!() variable to the crate code,
+    // so the workaround is to write it to a file for include_str!().
+    // Details: https://github.com/rust-lang/cargo/issues/2875
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let path = Path::new(&out_dir).join(kind);
+    let mut file = File::create(&path)?;
+    file.write_all(data.as_bytes())
+}
+
 
 fn git_head_sha() -> Result<String, Box<Error>> {
     let mut cmd = Command::new("git");
@@ -36,4 +41,13 @@ fn git_head_sha() -> Result<String, Box<Error>> {
     let output = try!(cmd.output());
     let sha = try!(str::from_utf8(&output.stdout[..])).trim().to_owned();
     Ok(sha)
+}
+
+fn compiler_signature() -> Result<String, Box<Error>> {
+    let rustc = rustc_version::version_meta()?;
+    let signature = format!("{channel} {version} on {host}",
+        version = rustc.short_version_string,
+        channel = format!("{:?}", rustc.channel).to_lowercase(),
+        host = rustc.host);
+    Ok(signature)
 }
