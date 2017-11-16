587a313ad1dce0e5b4cc57fc0a23d875f0cbfabe
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2017-11-16 23:01:53 +0100

    Less (modules) is more

--- src/cli/args/mod.rs
+++ src/cli/args/mod.rs
@@ -2,17 +2,23 @@
 
 mod image_macro;
 mod model;
-mod parser;
+
+#[cfg(test)]
+mod tests;
 
 
 use std::env;
 use std::ffi::OsString;
+use std::io;
+use std::path::PathBuf;
 
 use conv::TryFrom;
+use clap::{self, AppSettings, Arg, ArgGroup, ArgMatches};
+use serde_json;
 
 use super::{NAME, VERSION};
 pub use self::model::{ArgsError, Options};
-use self::parser::create_parser;
+use self::image_macro::parse as parse_image_macro;
 
 
 /// Parse command line arguments and return `Options` object.
@@ -33,129 +39,113 @@ pub fn parse_from_argv<I, T>(argv: I) -> Result<Options, ArgsError>
 }
 
 
-#[cfg(test)]
-mod tests {
-    use rofl::{HAlign, VAlign};
-    use spectral::prelude::*;
-    use super::parse_from_argv;
-    use ::NAME;
+impl<'a> TryFrom<ArgMatches<'a>> for Options {
+    type Err = ArgsError;
 
-    #[test]
-    fn no_args() {
-        assert_that!(parse_from_argv(Vec::<&str>::new())).is_err();
-        assert_that!(parse_from_argv(vec![*NAME])).is_err();
+    fn try_from(matches: ArgMatches<'a>) -> Result<Self, Self::Err> {
+        let verbose_count = matches.occurrences_of(OPT_VERBOSE) as isize;
+        let quiet_count = matches.occurrences_of(OPT_QUIET) as isize;
+        let verbosity = verbose_count - quiet_count;
+
+        let image_macro = match matches.value_of(ARG_MACRO) {
+            Some(im) => parse_image_macro(im.trim())?,
+            None => {
+                assert!(matches.is_present(OPT_JSON),
+                    "Command line incorrectly parsed without either `{}` argument or --{} flag",
+                    ARG_MACRO, OPT_JSON);
+                serde_json::from_reader(&mut io::stdin())?
+            }
+        };
+
+        // Output path can be set explicitly to stdout via `-`.
+        let output_path = matches.value_of(OPT_OUTPUT)
+            .map(|p| p.trim())
+            .and_then(|p| if p == "-" { None } else { Some(p) })
+            .map(|p| PathBuf::from(p));
+
+        Ok(Options{verbosity, image_macro, output_path})
     }
+}
 
-    #[test]
-    fn macro_just_template() {
-        let opts = parse_from_argv(vec![*NAME, "zoidberg{Test}"]).unwrap();
-        assert_eq!("zoidberg", opts.image_macro.template);
-        assert_eq!("Test", opts.image_macro.captions[0].text);
+
+// Parser definition
+
+/// Type of the argument parser object
+/// (which is called an "App" in clap's silly nomenclature).
+pub type Parser<'p> = clap::App<'p, 'p>;
+
+
+lazy_static! {
+    static ref ABOUT: &'static str = option_env!("CARGO_PKG_DESCRIPTION").unwrap_or("");
+}
+
+const ARGGRP_MACRO: &'static str = "image_macro";
+const ARG_MACRO: &'static str = "macro";
+const OPT_JSON: &'static str = "json";
+const OPT_OUTPUT: &'static str = "output";
+const OPT_VERBOSE: &'static str = "verbose";
+const OPT_QUIET: &'static str = "quiet";
+
+
+/// Create the parser for application's command line.
+pub fn create_parser<'p>() -> Parser<'p> {
+    let mut parser = Parser::new(*NAME);
+    if let Some(version) = *VERSION {
+        parser = parser.version(version);
     }
+    parser
+        .about(*ABOUT)
+        .author(crate_authors!(", "))
 
-    #[test]
-    fn macro_one_text() {
-        let opts = parse_from_argv(vec![*NAME, "zoidberg{Test}"]).unwrap();
-        assert_eq!("zoidberg", opts.image_macro.template);
-        let caption = &opts.image_macro.captions[0];
-        assert_eq!("Test", caption.text);
-        assert_eq!(VAlign::Bottom, caption.valign);
-        assert_eq!(HAlign::Center, caption.halign);
-    }
+        .setting(AppSettings::StrictUtf8)
 
-    #[test]
-    fn macro_two_texts() {
-        let opts = parse_from_argv(vec![*NAME, "zoidberg{Test1}{Test2}"]).unwrap();
-        assert_eq!("zoidberg", opts.image_macro.template);
+        .setting(AppSettings::UnifiedHelpMessage)
+        .setting(AppSettings::DontCollapseArgsInUsage)
+        .setting(AppSettings::DeriveDisplayOrder)
 
-        let caption1 = &opts.image_macro.captions[0];
-        let caption2 = &opts.image_macro.captions[1];
+        // Image macro specification.
+        .group(ArgGroup::with_name(ARGGRP_MACRO)
+            .args(&[ARG_MACRO, OPT_JSON])
+            .required(true))  // TODO: make it optional and add interactive option
+        .arg(Arg::with_name(ARG_MACRO)
+            .value_name("MACRO")
+            .help("Image macro to render")
+            .long_help(concat!(
+                "Specification of the image macro to render.\n\n",
+                "The syntax is: TEMPLATE{CAPTION}{CAPTION}..., where CAPTION is just text ",
+                "or text preceded by alignment symbols: ^, - (middle), _ (bottom), ",
+                "<, | (center), >.")))
+        .arg(Arg::with_name(OPT_JSON)
+            .conflicts_with(ARG_MACRO)
+            .long("json").short("j")
+            .help("Whether to expect image macro as JSON on standard input")
+            .long_help(concat!(
+                "If present, the image macro specification will be read as JSON ",
+                "from the program's standard input.")))
+            // TODO: some documentation of the JSON format
 
-        assert_eq!("Test1", caption1.text);
-        assert_eq!("Test2", caption2.text);
-        // TODO: the valign should be Top+Bottom here but this intelligent valign
-        // picking is NYI
-        assert_eq!(HAlign::Center, caption1.halign);
-        assert_eq!(HAlign::Center, caption2.halign);
-    }
+        // Output flags.
+        .arg(Arg::with_name(OPT_OUTPUT)
+            .long("output").short("o")
+            .required(false)
+            .help("File to write the rendered image to")
+            .long_help(concat!(
+                "What file should the final image be written to.\n\n",
+                "By default, or when this flag is set to `-` (single dash), the image is written ",
+                "to standard output so it can be e.g. piped to the ImageMagick `display` program.")))
 
-    #[test]
-    fn macro_text_just_valign() {
-        let opts = parse_from_argv(vec![*NAME, "zoidberg{^Test}"]).unwrap();
-        assert_eq!("zoidberg", opts.image_macro.template);
-        let caption = &opts.image_macro.captions[0];
-        assert_eq!("Test", caption.text);
-        assert_eq!(VAlign::Top, caption.valign);
-    }
+        // Verbosity flags.
+        .arg(Arg::with_name(OPT_VERBOSE)
+            .long("verbose").short("v")
+            .multiple(true)
+            .conflicts_with(OPT_QUIET)
+            .help("Increase logging verbosity"))
+        .arg(Arg::with_name(OPT_QUIET)
+            .long("quiet").short("q")
+            .multiple(true)
+            .conflicts_with(OPT_VERBOSE)
+            .help("Decrease logging verbosity"))
 
-    #[test]
-    fn macro_text_just_halign() {
-        let opts = parse_from_argv(vec![*NAME, "zoidberg{>Test}"]).unwrap();
-        assert_eq!("zoidberg", opts.image_macro.template);
-        let caption = &opts.image_macro.captions[0];
-        assert_eq!("Test", caption.text);
-        assert_eq!(HAlign::Right, caption.halign);
-    }
-
-    #[test]
-    fn macro_text_valign_and_halign() {
-        let opts = parse_from_argv(vec![*NAME, "zoidberg{-<Test}"]).unwrap();
-        assert_eq!("zoidberg", opts.image_macro.template);
-        let caption = &opts.image_macro.captions[0];
-        assert_eq!("Test", caption.text);
-        assert_eq!(VAlign::Middle, caption.valign);
-        assert_eq!(HAlign::Left, caption.halign);
-    }
-
-    #[test]
-    fn macro_text_halign_and_valign() {
-        let opts = parse_from_argv(vec![*NAME, "zoidberg{|^Test}"]).unwrap();
-        assert_eq!("zoidberg", opts.image_macro.template);
-        let caption = &opts.image_macro.captions[0];
-        assert_eq!("Test", caption.text);
-        assert_eq!(HAlign::Center, caption.halign);
-        assert_eq!(VAlign::Top, caption.valign);
-    }
-
-    #[test]
-    fn macro_full() {
-        let opts = parse_from_argv(vec![
-            *NAME, "zoidberg{<^Need a test?}{_>Why not Zoidberg?}"]).unwrap();
-        assert_eq!("zoidberg", opts.image_macro.template);
-
-        let caption1 = &opts.image_macro.captions[0];
-        assert_eq!("Need a test?", caption1.text);
-        assert_eq!(HAlign::Left, caption1.halign);
-        assert_eq!(VAlign::Top, caption1.valign);
-
-        let caption2 = &opts.image_macro.captions[1];
-        assert_eq!("Why not Zoidberg?", caption2.text);
-        assert_eq!(VAlign::Bottom, caption2.valign);
-        assert_eq!(HAlign::Right, caption2.halign);
-    }
-
-    #[test]
-    fn macro_error_no_template() {
-        assert_that!(parse_from_argv(vec![*NAME, "{}"])).is_err();
-        assert_that!(parse_from_argv(vec![*NAME, "{Test}"])).is_err();
-    }
-
-    #[test]
-    fn macro_error_unclosed_brace() {
-        assert_that!(parse_from_argv(vec![*NAME, "zoidberg{Test"])).is_err();
-    }
-
-    #[test]
-    fn macro_error_nested_braces() {
-        assert_that!(parse_from_argv(vec![*NAME, "zoidberg{Test{More tests}}"]))
-            .is_err();
-    }
-
-    #[test]
-    fn macro_error_closing_brace_first() {
-        assert_that!(parse_from_argv(vec![*NAME, "zoidberg}"])).is_err();
-    }
-
-    // TODO: test the --json flag (which is actually difficult because it requires mocking
-    // or DI'ing or otherwise seeding the stdin with JSON)
+        .help_short("H")
+        .version_short("V")
 }
---
+++ src/cli/args/tests.rs
@@ -0,0 +1,129 @@
+//! Tests for command line argument handling.
+
+use rofl::{HAlign, VAlign};
+use spectral::prelude::*;
+
+use super::parse_from_argv;
+use ::NAME;
+
+
+#[test]
+fn no_args() {
+    assert_that!(parse_from_argv(Vec::<&str>::new())).is_err();
+    assert_that!(parse_from_argv(vec![*NAME])).is_err();
+}
+
+#[test]
+fn macro_just_template() {
+    let opts = parse_from_argv(vec![*NAME, "zoidberg{Test}"]).unwrap();
+    assert_eq!("zoidberg", opts.image_macro.template);
+    assert_eq!("Test", opts.image_macro.captions[0].text);
+}
+
+#[test]
+fn macro_one_text() {
+    let opts = parse_from_argv(vec![*NAME, "zoidberg{Test}"]).unwrap();
+    assert_eq!("zoidberg", opts.image_macro.template);
+    let caption = &opts.image_macro.captions[0];
+    assert_eq!("Test", caption.text);
+    assert_eq!(VAlign::Bottom, caption.valign);
+    assert_eq!(HAlign::Center, caption.halign);
+}
+
+#[test]
+fn macro_two_texts() {
+    let opts = parse_from_argv(vec![*NAME, "zoidberg{Test1}{Test2}"]).unwrap();
+    assert_eq!("zoidberg", opts.image_macro.template);
+
+    let caption1 = &opts.image_macro.captions[0];
+    let caption2 = &opts.image_macro.captions[1];
+
+    assert_eq!("Test1", caption1.text);
+    assert_eq!("Test2", caption2.text);
+    // TODO: the valign should be Top+Bottom here but this intelligent valign
+    // picking is NYI
+    assert_eq!(HAlign::Center, caption1.halign);
+    assert_eq!(HAlign::Center, caption2.halign);
+}
+
+#[test]
+fn macro_text_just_valign() {
+    let opts = parse_from_argv(vec![*NAME, "zoidberg{^Test}"]).unwrap();
+    assert_eq!("zoidberg", opts.image_macro.template);
+    let caption = &opts.image_macro.captions[0];
+    assert_eq!("Test", caption.text);
+    assert_eq!(VAlign::Top, caption.valign);
+}
+
+#[test]
+fn macro_text_just_halign() {
+    let opts = parse_from_argv(vec![*NAME, "zoidberg{>Test}"]).unwrap();
+    assert_eq!("zoidberg", opts.image_macro.template);
+    let caption = &opts.image_macro.captions[0];
+    assert_eq!("Test", caption.text);
+    assert_eq!(HAlign::Right, caption.halign);
+}
+
+#[test]
+fn macro_text_valign_and_halign() {
+    let opts = parse_from_argv(vec![*NAME, "zoidberg{-<Test}"]).unwrap();
+    assert_eq!("zoidberg", opts.image_macro.template);
+    let caption = &opts.image_macro.captions[0];
+    assert_eq!("Test", caption.text);
+    assert_eq!(VAlign::Middle, caption.valign);
+    assert_eq!(HAlign::Left, caption.halign);
+}
+
+#[test]
+fn macro_text_halign_and_valign() {
+    let opts = parse_from_argv(vec![*NAME, "zoidberg{|^Test}"]).unwrap();
+    assert_eq!("zoidberg", opts.image_macro.template);
+    let caption = &opts.image_macro.captions[0];
+    assert_eq!("Test", caption.text);
+    assert_eq!(HAlign::Center, caption.halign);
+    assert_eq!(VAlign::Top, caption.valign);
+}
+
+#[test]
+fn macro_full() {
+    let opts = parse_from_argv(vec![
+        *NAME, "zoidberg{<^Need a test?}{_>Why not Zoidberg?}"]).unwrap();
+    assert_eq!("zoidberg", opts.image_macro.template);
+
+    let caption1 = &opts.image_macro.captions[0];
+    assert_eq!("Need a test?", caption1.text);
+    assert_eq!(HAlign::Left, caption1.halign);
+    assert_eq!(VAlign::Top, caption1.valign);
+
+    let caption2 = &opts.image_macro.captions[1];
+    assert_eq!("Why not Zoidberg?", caption2.text);
+    assert_eq!(VAlign::Bottom, caption2.valign);
+    assert_eq!(HAlign::Right, caption2.halign);
+}
+
+#[test]
+fn macro_error_no_template() {
+    assert_that!(parse_from_argv(vec![*NAME, "{}"])).is_err();
+    assert_that!(parse_from_argv(vec![*NAME, "{Test}"])).is_err();
+}
+
+#[test]
+fn macro_error_unclosed_brace() {
+    assert_that!(parse_from_argv(vec![*NAME, "zoidberg{Test"])).is_err();
+}
+
+#[test]
+fn macro_error_nested_braces() {
+    assert_that!(parse_from_argv(vec![*NAME, "zoidberg{Test{More tests}}"]))
+        .is_err();
+}
+
+#[test]
+fn macro_error_closing_brace_first() {
+    assert_that!(parse_from_argv(vec![*NAME, "zoidberg}"])).is_err();
+}
+
+// TODO: test the --json flag (which is actually difficult because it requires mocking
+// or DI'ing or otherwise seeding the stdin with JSON);
+// alternatively, we need an intermediate structure between clap::ArgMatches
+// and Options that contains the matches + the content of stdin
